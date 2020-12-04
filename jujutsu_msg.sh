13dabbc9a8e3690727c51a9efaad086232f28bb8
Evelyn Correa <evelynfcorrea@gmail.com>
2020-12-04 17:48:27 -0300

    Update jujutsu_msg.sh

--- jujutsu_msg.sh
+++ jujutsu_msg.sh
@@ -1,15 +1,12 @@
-# Webhook URL
 discord_url="https://discord.com/api/webhooks/784511643504869386/uDN2FFgwbGP-g69c9qV7Argr98UXcUIqn3OreAVrnkoalnnSvIxZis_Uk9xeQkPT67Bk"
 
 generate_post_data() {
   cat <<EOF
 {
-  "content": "JUJUTSU!!!!!!!!!!",
+  "content": "Amigos <@&782082777528008724>, atenção:",
   "embeds": [{
     "title": "Alô alô leia com atenção <:jujutsu_feira_5:784510037879816264>",
-    "description": "Amigos <@&782082777528008724>! Hoje é dia de Jujutsu Kaisen! Vamos nos reunir para assistir no canal de voz da category *JUJUTSU-FEIRA* *à partir das **21h**!* :smirk-cat: \n
-    **ATENÇÃO!** \n
-    Pessoal importante dorme cedo, se não aparecer até 23h, vamos começar sem você :grimacing:",
+    "description": "Amigos! Hoje é dia de Jujutsu Kaisen! Vamos nos reunir para assistir no canal de voz da category *JUJUTSU-FEIRA* *à partir das **21h**!* :smirk_cat: \n**ATENÇÃO!** \nPessoal dorme cedo, se não aparecer até 23h, vamos começar sem você :grimacing:",
     "color": "05555"
   }]
 }
